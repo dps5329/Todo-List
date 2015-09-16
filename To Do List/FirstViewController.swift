8368d2623dadfe10f947858ccbf4680f54af3947
Daniel Schartner <danielpschartner@gmail.com>
2015-09-16 10:38:31 -0400

    First commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var allReminders = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var reminderTable: UITableView!
+    
+    //set the reminders array to array in memory
+    func reloadData(){
+    if(NSUserDefaults.standardUserDefaults().objectForKey("reminders") != nil){
+            allReminders = NSUserDefaults.standardUserDefaults().objectForKey("reminders") as [String]
+        }
+    }
+    
+    //use the number of elements in array as the number of rows
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return allReminders.count
+    }
+    
+    //load the data in the array into the table cells
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "reminderCell")
+        
+        cell.textLabel?.text = allReminders[indexPath.row]
+
+        return cell
+    }
+    
+    //allow editing
+    func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        return true
+    }
+    
+    //handle delete
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // handle delete
+            println(indexPath.row)
+            
+            if(indexPath.row < allReminders.count){
+                allReminders.removeAtIndex(indexPath.row)
+            }
+            
+            NSUserDefaults.standardUserDefaults().setObject(allReminders, forKey: "reminders")
+            
+            reminderTable.reloadData()
+        }
+    }
+    
+    //reload when user switches back to first page
+    override func viewDidAppear(animated: Bool) {
+        reloadData()
+        reminderTable.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        reloadData()
     }
 
     override func didReceiveMemoryWarning() {
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userInput: UITextField!
+    
+    //add new item to reminders array
+    @IBAction func newReminder(sender: AnyObject) {
+        if(userInput.text != ""){
+            
+            allReminders.append(userInput.text)
+            
+            NSUserDefaults.standardUserDefaults().setObject(allReminders, forKey: "reminders")
+        }
+        
+        userInput.text = ""
+    }
+   
+    //handle keyboard
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.userInput.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
